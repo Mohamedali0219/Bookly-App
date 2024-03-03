271a9967258e92e46ff6dd1d95a357906a5b428b
Mohamedali0219 <Mohamedali.D2002@gmail.com>
2024-03-03 09:25:40 +0200

    spalsh view body

---
+++ lib/Features/splash/peresentation/views/spalsh_view.dart
@@ -0,0 +1,13 @@
+import 'package:bookly_app/Features/splash/peresentation/views/widgets/spalsh_view_body.dart';
+import 'package:flutter/material.dart';
+
+class SpalshView extends StatelessWidget {
+  const SpalshView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SpalshViewBody(),
+    );
+  }
+}
