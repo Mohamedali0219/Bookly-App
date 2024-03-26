501b0158a4663c96df63542cce904380b0064a99
Mohamedali0219 <Mohamedali.D2002@gmail.com>
2024-03-26 06:36:06 +0200

    implement featch similar books

--- lib/Features/home/data/repos/home_repo.dart
+++ lib/Features/home/data/repos/home_repo.dart
@@ -5,4 +5,7 @@ import 'package:dartz/dartz.dart';
 abstract class HomeRepo {
   Future<Either<Fauiler, List<BookModel>>> fetchFeaturedBooks();
   Future<Either<Fauiler, List<BookModel>>> fetchNewestBooks();
+  Future<Either<Fauiler, List<BookModel>>> fetchSimilarBooks({
+    required String category,
+  });
 }
